9dbc95b04dd854a1c3a8b28c1a7278282da71dcd
StoreTunes <58260646+StoreTunes@users.noreply.github.com>
2025-02-02 20:23:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ def index():
     return render_template("index.html", tracking_data=tracking_data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
